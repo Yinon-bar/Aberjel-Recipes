36f43d590470fcadb21d25100f33ed09dd13864c
Yinon Bar <yinonbar1988@gmail.com>
2024-08-16 19:45:19 +0300

    Featured image

--- wp-content/themes/shoshana-recipes/archive.php
+++ wp-content/themes/shoshana-recipes/archive.php
@@ -4,15 +4,15 @@
 $obj = get_queried_object();
 $cat = $obj->name;
 $category_recipes = new WP_Query(array(
-    'posts_per_page' => -1,
-    'post_type' => 'recipes',
-    'tax_query' => array(
-        array(
-            'taxonomy' => 'type',
-            'field' => 'slug',
-            'terms' => $cat,
-        )
+  'posts_per_page' => -1,
+  'post_type' => 'recipes',
+  'tax_query' => array(
+    array(
+      'taxonomy' => 'type',
+      'field' => 'slug',
+      'terms' => $cat,
     )
+  )
 ));
 $desc = category_description();
 // echo "<pre>";
@@ -22,40 +22,40 @@ $desc = category_description();
 ?>
 
 <div class="archive">
-    <div class="page-banner" style="background: linear-gradient(rgb(0, 0, 0, 0.6), rgb(0, 0, 0, 0.6)),
+  <div class="page-banner" style="background: linear-gradient(rgb(0, 0, 0, 0.6), rgb(0, 0, 0, 0.6)),
                     url(<?php if (function_exists('z_taxonomy_image_url')) echo z_taxonomy_image_url(); ?>);">
-        <div class="container">
-            <h2><?php echo the_field('subtitle', $obj) ?></h2>
-            <p><?php echo $desc ?></p>
-            <form method="get" action="<?php echo esc_url(site_url('/')) ?>">
-                <input type="search" name="s" id="" placeholder="הקלד מילה לחיפוש">
-                <input type="submit" value="חיפוש מתכון">
-            </form>
-        </div>
-    </div>
     <div class="container">
-        <section class="newest-recipes">
-            <div class="cards">
-                <?php
-                while ($category_recipes->have_posts()) {
-                    $category_recipes->the_post(); ?>
-                    <div class="card">
-                        <?php $image = get_field('recipe_image') ?>
-                        <div class="card-image" style="background-image: url(<?php echo esc_url($image[0]['url']); ?>);">
-                        </div>
-                        <div class="card-body">
-                            <h3><?php the_field('title') ?></h3>
-                            <div class="badge"><span class="time"><?php the_field('time') ?></span>
-                                דקות הכנה
-                            </div>
-                            <a class="link" href="<?php the_permalink() ?>">קרא עוד</a>
-                        </div>
-                    </div>
-                <?php }
-                ?>
-            </div>
-        </section>
+      <h2><?php echo $obj->name ?></h2>
+      <p style="width: 450px;"><?php echo $desc ?></p>
+      <form method="get" action="<?php echo esc_url(site_url('/')) ?>">
+        <input type="search" name="s" id="" placeholder="הקלד מילה לחיפוש">
+        <input type="submit" value="חיפוש מתכון">
+      </form>
     </div>
+  </div>
+  <div class="container">
+    <section class="newest-recipes">
+      <div class="cards">
+        <?php
+        while ($category_recipes->have_posts()) {
+          $category_recipes->the_post(); ?>
+          <div class="card">
+            <?php $image = get_field('recipe_image') ?>
+            <div class="card-image" style="background-image: url(<?php echo esc_url($image[0]['url']); ?>);">
+            </div>
+            <div class="card-body">
+              <h3><?php the_field('title') ?></h3>
+              <div class="badge"><span class="time"><?php the_field('time') ?></span>
+                דקות הכנה
+              </div>
+              <a class="link" href="<?php the_permalink() ?>">קרא עוד</a>
+            </div>
+          </div>
+        <?php }
+        ?>
+      </div>
+    </section>
+  </div>
 </div>
 
 
--- wp-content/themes/shoshana-recipes/header.php
+++ wp-content/themes/shoshana-recipes/header.php
@@ -2,30 +2,29 @@
 <html lang="he">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head() ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head() ?>
 </head>
 
 <body <?php body_class() ?>>
-
-    <header class="header">
-        <nav class="transparent-nav">
-            <?php wp_nav_menu('header-menu') ?>
-        </nav>
-        <?php
-        if ($pageBanner = get_field('page_image')) { ?>
-            <div class="page-banner" style="background: linear-gradient(rgb(0, 0, 0, 0.6), rgb(0, 0, 0, 0.6)),
+  <header class="header">
+    <nav class="transparent-nav">
+      <?php wp_nav_menu('header-menu') ?>
+    </nav>
+    <?php
+    if ($pageBanner = get_field('page_image')) { ?>
+      <div class="page-banner" style="background: linear-gradient(rgb(0, 0, 0, 0.6), rgb(0, 0, 0, 0.6)),
                 url(<?php echo $pageBanner['sizes']['banner'] ?>);">
-                <div class="container">
-                    <h3><?php the_field('page_headline') ?></h3>
-                    <p><?php the_field('page_banner_paragraph'); ?></p>
-                    <form method="get" action="<?php echo esc_url(site_url('/')) ?>">
-                        <input type="search" name="s" id="" placeholder="הקלד מילה לחיפוש">
-                        <input type="submit" value="חיפוש מתכון">
-                    </form>
-                </div>
-            </div>
-        <?php };
-        ?>
-    </header>
+        <div class="container">
+          <h3><?php the_field('page_headline') ?></h3>
+          <p><?php the_field('page_banner_paragraph'); ?></p>
+          <form method="get" action="<?php echo esc_url(site_url('/')) ?>">
+            <input type="search" name="s" id="" placeholder="הקלד מילה לחיפוש">
+            <input type="submit" value="חיפוש מתכון">
+          </form>
+        </div>
+      </div>
+    <?php };
+    ?>
+  </header>
